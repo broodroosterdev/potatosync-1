a6d9be3f573176fcea8c4fc2ac69495a543a10b5
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2020-10-12 10:18:37 +0200

    Update general.ts

--- src/routes/general.ts
+++ src/routes/general.ts
@@ -3,20 +3,4 @@ import { general } from '../controller';
 
 const router = new SwaggerRouter();
 
-// include all routes
-router.use('/', general.routes()).use(general.allowedMethods());
-router.use('/user', user.routes()).use(user.allowedMethods());
-
-
-// Swagger endpoint
-router.swagger({
-  title: 'PotatoSync',
-  description: 'PotatoSync REST API',
-  version: '2.0.0',
-  prefix: '/api/v2/login'
-});
-
-// mapDir will scan the input dir, and automatically call router.map to all Router Class
-router.mapDir(path.join(__dirname, '../'));
-
 export { router as general };
